fd94bbeb49087fa5a9004da44562da31ff578c45
vincemuller <vmuller2529@gmail.com>
2025-05-17 20:39:06 -0700

    Initial Commit

---
+++ CropCurious/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CropCurious
+//
+//  Created by Vince Muller on 5/17/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CropCurious/CropCuriousApp.swift
@@ -0,0 +1,17 @@
+//
+//  CropCuriousApp.swift
+//  CropCurious
+//
+//  Created by Vince Muller on 5/17/25.
+//
+
+import SwiftUI
+
+@main
+struct CropCuriousApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
